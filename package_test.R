b1152771d2c49e4a69e6e986b215937fe9b3fe4e
sword93 <sword93@gmail.com>
2019-01-25 14:05:36 +0900

    Create package_test.R

---
+++ package_test.R
@@ -0,0 +1,5 @@
+library("data.table") # 파일 입출력
+library("dplyr")      # 데이터 핸들링
+library("reshape2")   # 데이터 핸들링
+library("agricolae")  # ANOVA 사후 검저(duncan.test)
+library("e1071")      # 머신러닝 + 첨도 + 왜도  
